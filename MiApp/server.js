b5b7f6545e05fdbb4e12f86232b218a3bcbf299f
Swain.L <liusw0719@gmail.com>
2018-01-20 13:53:54 +0800

    添加项目文件。

---
+++ MiApp/server.js
@@ -0,0 +1,27 @@
+'use strict';
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (request, response) {
+    if (request.url === "/index") {
+        fs.readFile("index.html", function (err, data) {
+            if (err) {
+                response.writeHead(404);
+                response.write("Not Found");
+            }
+            else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(data);
+                response.end();
+            }
+        });
+    } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
+        response.end();
+    }
+}).listen(3000);
+
+
+
+
